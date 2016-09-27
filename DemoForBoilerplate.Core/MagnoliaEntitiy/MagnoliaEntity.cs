33b98c26b356d9dca9a10095398fe00d95090ca9
zhaokuo <1014336691@qq.com>
2016-09-27 15:25:03 +0800

    Revert "Revert "添加一个测试实体, 用于测试Magnolia脚手架使用情况  :  添加实体与 上下文""
    This reverts commit 6a8d787eafbecb4908081eba7065ff34b7ef66aa.

---
+++ DemoForBoilerplate.Core/MagnoliaEntitiy/MagnoliaEntity.cs
@@ -0,0 +1,18 @@
+﻿using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoForBoilerplate.MagnoliaEntitiy
+{
+	public class MagnoliaEntity : FullAuditedEntity
+	{
+
+		public virtual int Number { get; set; }
+		public virtual string Intro { get; set; }
+		public virtual List<string> Tags { get; set; }
+
+	}
+}
